280400ed38ef93581ef8348a4dbc3e2bbc363917
Code Sandbox <hello@codesandbox.io>
2022-02-15 20:46:48 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.eace8c6280409a44b0fae55a12222c19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "487c70731a010b4005d0c5ab4f0dabb8",
+    "url": "https://mikkail04.github.io/csb-koko1/index.html"
+  },
+  {
+    "revision": "4a40175c67f113e45b29",
+    "url": "https://mikkail04.github.io/csb-koko1/static/css/main.9ee66982.chunk.css"
+  },
+  {
+    "revision": "bcf9a36e09c51a3fd9ce",
+    "url": "https://mikkail04.github.io/csb-koko1/static/js/2.6012264f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "https://mikkail04.github.io/csb-koko1/static/js/2.6012264f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a40175c67f113e45b29",
+    "url": "https://mikkail04.github.io/csb-koko1/static/js/main.842dc115.chunk.js"
+  },
+  {
+    "revision": "aaf5476730711bc82de1",
+    "url": "https://mikkail04.github.io/csb-koko1/static/js/runtime-main.fb128e85.js"
+  }
+]);
